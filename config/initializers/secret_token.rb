b2853526ec42df783df4fa7dff025e19c9d8c36e
tyeh <tyeh@thecooldoc.com>
2013-08-30 09:37:20 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sraid::Application.config.secret_key_base = '5c651efc746c93a7793a127657e6debdee3b691da9b243355b2ad7c65a53d3f0d1c3309042f319acaa535105048d1674de2ab166b915f24d11ee0918b8df7a9c'
